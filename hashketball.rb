e54097e91f7b76b0443cb1d72bc90fb3022f305a
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-06 20:03:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,14 +5,14 @@ def game_hash
    :home => {
      :team_name => "",
      :color => [],
-     :players => {}
+     :players => []
    },
    
    
    :away => {
      :team_name => "",
      :color => [],
-     :players => {}
+     :players => []
    }
     
   }
