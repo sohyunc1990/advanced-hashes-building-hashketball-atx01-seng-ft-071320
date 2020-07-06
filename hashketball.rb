8b055525ab6d3f6879e944d71e3b229eee8d35df
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-06 20:22:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -67,7 +67,7 @@ def game_hash
      :colors => ["Turquoise", "Purple"],
       :players => [{
        :player_name => "Jeff Adrien",
-       :number => "4",
+       :number => 4,
        :shoe => "18",
        :points => "10",
        :rebounds => "1",
