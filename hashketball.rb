723067f5b877bc428c5ddca16321f309b471e684
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-06 20:12:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,15 +17,15 @@ def game_hash
        :slam_dunks => "1"
        }
        {
-       :player_name => "Alan Anderson",
-       :number => "0",
-       :shoe => "16",
-       :points => "22" ,
+       :player_name => "Reggie Evans",
+       :number => "30",
+       :shoe => "14",
+       :points => "12" ,
        :rebounds => "12",
        :assists => "12",
-       :steals => "3",
-       :blocks => "1",
-       :slam_dunks => "1"
+       :steals => "12",
+       :blocks => "12",
+       :slam_dunks => "7"
        }]
    },
    
