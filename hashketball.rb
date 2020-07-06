7685b517136a4cbac1395363ae481e8ed2c73bb1
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-06 20:15:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -39,14 +39,25 @@ def game_hash
        :slam_dunks => "15"
        },
        {
-       :player_name => "Alan Anderson",
-       :number => "0",
-       :shoe => "16",
-       :points => "22" ,
-       :rebounds => "12",
-       :assists => "12",
+       :player_name => "Mason Plumlee",
+       :number => "1",
+       :shoe => "19",
+       :points => "26" ,
+       :rebounds => "11",
+       :assists => "6",
        :steals => "3",
-       :blocks => "1",
+       :blocks => "8",
+       :slam_dunks => "5"
+       },
+       {
+       :player_name => "Jason Terry",
+       :number => "31",
+       :shoe => "15",
+       :points => "19" ,
+       :rebounds => "2",
+       :assists => "2",
+       :steals => "4",
+       :blocks => "11",
        :slam_dunks => "1"
        }]
    },
