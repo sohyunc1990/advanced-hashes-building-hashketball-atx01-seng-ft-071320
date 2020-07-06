deeea1f16b6102a604bda03e526566d835262c02
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-06 20:14:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -37,6 +37,17 @@ def game_hash
        :steals => "3",
        :blocks => "1",
        :slam_dunks => "15"
+       },
+       {
+       :player_name => "Alan Anderson",
+       :number => "0",
+       :shoe => "16",
+       :points => "22" ,
+       :rebounds => "12",
+       :assists => "12",
+       :steals => "3",
+       :blocks => "1",
+       :slam_dunks => "1"
        }]
    },
    
