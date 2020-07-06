6df9efe132b92199937345ec495242189b8e6baf
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-06 20:22:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,7 @@ def game_hash
      :colors => ["Black", "White"],
      :players => [{
        :player_name => "Alan Anderson",
-       :number => "0",
+       :number => 0,
        :shoe => "16",
        :points => "22" ,
        :rebounds => "12",
