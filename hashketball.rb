59423a3c7ec813d338a669f8f911cd70dfcbaaab
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-06 20:10:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,9 +6,9 @@ def game_hash
      :team_name => "Brooklyn Nets",
      :color => ["Black", "White"],
      :players => [{
-       :player_name => "Alan Anderson" ,
-       :number => "0" ,
-       :shoe => "16" ,
+       :player_name => "Alan Anderson",
+       :number => "0",
+       :shoe => "16",
        :points => "22" ,
        :rebounds => "12",
        :assists => "12",
@@ -20,18 +20,18 @@ def game_hash
    
    
    :away => {
-     :team_name => "",
-     :color => [],
+     :team_name => "Charlotte Hornets",
+     :color => ["Turquoise", "Purple"],
       :players => [{
-       :player_name => ,
-       :number => ,
-       :shoe => ,
-       :points => ,
-       :rebounds => ,
-       :assists => ,
-       :steals => ,
-       :blocks => ,
-       :slam_dunks =>
+       :player_name => "Jeff Adrien" ,
+       :number => "4",
+       :shoe => "18",
+       :points => "10",
+       :rebounds => "1",
+       :assists => "1",
+       :steals => "2",
+       :blocks => "7",
+       :slam_dunks => "2"
        }]
    }
     
